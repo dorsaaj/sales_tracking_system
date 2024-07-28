34fa3cccf1f505a5515f793799e82d3a0035abe4
dajami3 <dajami3@gatech.edu>
2024-07-28 18:20:36 -0400

    add files

---
+++ src/app/salesperson.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalespersonService {
+  private apiUrl = 'http://localhost:8000/salespersons';  // Adjust the URL if needed
+
+  constructor(private http: HttpClient) {}
+
+  getSalesperson(FirstName: string, LastName: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?FirstName=${FirstName}&LastName=${LastName}`);
+  }
+
+  createSalesperson(salesperson: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, salesperson);
+  }
+
+  deleteSalesperson(FirstName: string, LastName: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}?FirstName=${FirstName}&LastName=${LastName}`);
+  }
+
+  updateSalesperson(salesperson: any): Observable<any> {
+    return this.http.put<any>(this.apiUrl, salesperson);
+  }
+}
